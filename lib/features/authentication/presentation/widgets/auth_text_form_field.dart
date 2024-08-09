bc7c7c2223e3e7c988959a2fc825d7dd55d23678
Paula Ashraf <paula.a.pham@gmail.com>
2024-08-09 14:39:23 +0300

    create AuthTextFormField widget

---
+++ lib/features/authentication/presentation/widgets/auth_text_form_field.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class AuthTextFormField extends StatelessWidget {
+  final AutovalidateMode autovalidateMode;
+  final TextEditingController controller;
+  final String hintText;
+  final IconData prefixIcon;
+  final IconData? suffixIcon;
+  final TextInputType? keyboardType;
+  final bool? obscureText;
+  final String? Function(String?) validator;
+  const AuthTextFormField({
+    super.key,
+    required this.autovalidateMode,
+    required this.controller,
+    required this.hintText,
+    required this.prefixIcon,
+    this.suffixIcon,
+    this.keyboardType,
+    this.obscureText = false,
+    required this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autovalidateMode: autovalidateMode,
+      controller: controller,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          borderSide: BorderSide.none,
+        ),
+        disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          borderSide: BorderSide.none,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          borderSide: BorderSide.none,
+        ),
+        filled: true,
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          borderSide: const BorderSide(
+            width: 1.5,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          borderSide: BorderSide.none,
+        ),
+        hintText: hintText,
+        prefixIcon: Icon(
+          prefixIcon,
+        ),
+        suffixIcon: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            suffixIcon,
+          ),
+        ),
+      ),
+      keyboardType: keyboardType,
+      obscureText: obscureText!,
+      validator: validator,
+    );
+  }
+}
