0828270efec37f9ed50611b3dd8e06aaccace5b0
Paula Ashraf <paula.a.pham@gmail.com>
2024-08-10 13:55:02 +0300

    create CustomOutlinedButton widget

---
+++ lib/core/widgets/custom_outlined_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomOutlinedButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+  const CustomOutlinedButton({
+    super.key,
+    required this.onPressed,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: onPressed,
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        fixedSize: const Size.fromHeight(45.0),
+      ),
+      child: Text(
+        text,
+      ),
+    );
+    ;
+  }
+}
